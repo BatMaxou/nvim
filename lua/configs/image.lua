236616c463ccb75d9bb3518cf62b007f66f0733a
BatMaxou <maximebatista.lycee@gmail.com>
2025-04-10 00:00:56 +0200

    :sparkles: Add Image Viewer

---
+++ lua/configs/image.lua
@@ -0,0 +1,13 @@
+return {
+  doc = {
+    inline = false,
+    float = true,
+    max_width = 80,
+    max_height = 40,
+  },
+  wo = {
+    winhighlight = {
+      FloatBorder = "Pearl",
+    },
+  },
+}
--- lua/configs/snacks.lua
+++ lua/configs/snacks.lua
@@ -4,5 +4,12 @@ return {
     dashboard = {},
     terminal = require("configs.terminal"),
     indent = require("configs.indent"),
+    image = require("configs.image"),
+    styles = {
+      snacks_image = {
+        relative = "editor",
+        col = -1,
+      },
+    },
   },
 }
