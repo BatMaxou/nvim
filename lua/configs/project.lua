669a359c4891e749ff96b18121c08869b70e02f0
Batmaxou <maximebatista.lycee@gmail.com>
2025-04-14 17:06:32 +0200

    :sparkles: Exlude paths for project plugin

--- lua/configs/project.lua
+++ lua/configs/project.lua
@@ -1,6 +1,24 @@
 return {
   "ahmedkhalf/project.nvim",
   init = function()
-    require("project_nvim").setup()
+    require("project_nvim").setup({
+      sync_root_with_cwd = true,
+      respect_buf_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = true,
+      },
+      exclude_dirs = {
+        "*/node_modules/*",
+        "*/vendor/*",
+        "*/web/themes/*",
+        "*/web/modules/*",
+        "*/web/profiles/*",
+        "*/web/sites/*",
+        "*/.aatis/*",
+        "*/.devops/*",
+        "*/.boing/*",
+      },
+    })
   end,
 }
