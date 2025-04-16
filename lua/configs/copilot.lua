22fbac571ea6573610adf241c1ceaf1f901a4a4f
Batmaxou <maximebatista.lycee@gmail.com>
2025-04-16 14:49:36 +0200

    :technologist: Change Copilot repo

--- lua/configs/copilot.lua
+++ lua/configs/copilot.lua
@@ -1,9 +1,27 @@
 return {
-  'github/copilot.vim',
-  lazy = false,
-  -- https://github.com/orgs/community/discussions/12426
-  keys = {
-    { '<Right>', '<Plug>(copilot-accept-word)', mode = 'i', desc = 'Accept Copilot word' },
-    { '<Tab>', '<Plug>(copilot-accept-line)', mode = 'i', desc = 'Accept Copilot line' },
+  -- 'github/copilot.vim',
+  -- lazy = false,
+  -- -- https://github.com/orgs/community/discussions/12426
+  -- keys = {
+  --   { '<Right>', '<Plug>(copilot-accept-word)', mode = 'i', desc = 'Accept Copilot word' },
+  --   { '<Tab>', '<Plug>(copilot-accept-line)', mode = 'i', desc = 'Accept Copilot line' },
+  -- },
+  "zbirenbaum/copilot.lua",
+  event = "InsertEnter",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      hide_during_completion = true,
+      keymap = {
+        accept = "<S-Tab>",
+        accept_word = "<S-Right>",
+        accept_line = "<Tab>",
+        next = "<S-Down>",
+        prev = "<S-Up>",
+      },
+    },
   },
 }
