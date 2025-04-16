51894b5afc6378c5bca5b9abff21fba74af9db61
Batmaxou <maximebatista.lycee@gmail.com>
2025-04-16 11:54:12 +0200

    :technologist: Surround keybindings

--- lua/configs/surround.lua
+++ lua/configs/surround.lua
@@ -3,4 +3,9 @@ return {
   version = "*",
   event = "VeryLazy",
   opts = {},
+  keys = {
+    { "sw", "<Plug>(nvim-surround-normal)iw", mode = { "n", "v" }, desc = "Add word surrounding" },
+    { "sd", "<Plug>(nvim-surround-delete)", mode = { "n", "v" }, desc = "Delete surrounding" },
+    { "sc", "<Plug>(nvim-surround-change)", mode = { "n", "v" }, desc = "Change surrounding" },
+  },
 }
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,35 +1,38 @@
 local map = vim.keymap.set
 
 ------------ general ------------
-map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
-map("i", "<C-e>", "<End>", { desc = "move end of line" })
-map("i", "<C-h>", "<Left>", { desc = "move left" })
-map("i", "<C-l>", "<Right>", { desc = "move right" })
-map("i", "<C-j>", "<Down>", { desc = "move down" })
-map("i", "<C-k>", "<Up>", { desc = "move up" })
+map("i", "<C-b>", "<ESC>^i", { desc = "Move beginning of line" })
+map("i", "<C-e>", "<End>", { desc = "Move end of line" })
+map("i", "<C-h>", "<Left>", { desc = "Move left" })
+map("i", "<C-l>", "<Right>", { desc = "Move right" })
+map("i", "<C-j>", "<Down>", { desc = "Move down" })
+map("i", "<C-k>", "<Up>", { desc = "Move up" })
 
-map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
-map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
-map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
-map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
+map("n", "<C-h>", "<C-w>h", { desc = "Switch window left" })
+map("n", "<C-l>", "<C-w>l", { desc = "Switch window right" })
+map("n", "<C-j>", "<C-w>j", { desc = "Switch window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "Switch window up" })
 
-map("n", "<C-s>", "<cmd>w<CR>", { desc = "general save file" })
-map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "general copy whole file" })
+map("n", "<C-s>", "<cmd>w<CR>", { desc = "General save file" })
+map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "General copy whole file" })
 
-map("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "toggle line number" })
-map("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "toggle relative number" })
+map("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "Toggle line number" })
+map("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "Toggle relative number" })
 
 map("n", "<S-b>", "e", { desc = "End Word" })
 
+------------ general ------------
+map("n", "<leader>,", "$a,", { desc = "Add ',' at the end of a line" })
+
 ------------ comment ------------
-map("n", "<leader>:", "gcc", { desc = "toggle comment", remap = true })
-map("v", "<leader>:", "gc", { desc = "toggle comment", remap = true })
+map("n", "<leader>:", "gcc", { desc = "Toggle comment", remap = true })
+map("v", "<leader>:", "gc", { desc = "Toggle comment", remap = true })
 
 ------------ oil ------------
 map("n", "-", "<cmd>Oil --float<CR>", { desc = "Open parent directory" })
 
 ------------ diagnostic ------------
-map("n", "<leader>ds", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "show diagnostics of the line" })
+map("n", "<leader>ds", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Show diagnostics of the line" })
 
 ------------ terminal ------------
 map({ "n", "t" }, "<A-i>", function() Snacks.terminal.toggle() end, { desc = "Toggle terminal" })
